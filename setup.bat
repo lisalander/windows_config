a040fe34085f41f47fdf39d05c3b3e1e06f0c64f
lisalander <44831734+lisalander@users.noreply.github.com>
2020-05-07 14:33:31 +0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,15 @@
 @echo off
 
+:: Add Sublime Text Icon to context menu
 reg add "HKCR\*\shell\Open with Sublime Text" /v Icon /t REG_SZ /d "C:\Program Files\Sublime Text 3\sublime_text.exe"
+
+
+:: Enable microphone access
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone /v Value /t REG_SZ /d Allow /f
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone\NonPackaged /v Value /t REG_SZ /d Allow /f
+
+
+
+:: Disable microphone access
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone /v Value /t REG_SZ /d Deny /f
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone\NonPackaged /v Value /t REG_SZ /d Deny /f
